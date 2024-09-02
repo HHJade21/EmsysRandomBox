ae449e90be94f5ebf6c1a4ae0af9eb1825f93522
Han Yoon Soo <gskids053@chungbuk.ac.kr>
2024-09-02 22:09:02 +0900

    Test
    일단 떨어지고 움직이는 시스템까지는 구현 성공했는데... 이후가 문제

--- Assets/Scripts/Manager/GenshinImpact.cs
+++ Assets/Scripts/Manager/GenshinImpact.cs
@@ -1,48 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
-using System.Diagnostics;
 using UnityEngine;
-using System.Threading.Tasks;
 
 namespace codingchildren
 {
     public class GenshinImpact : MonoBehaviour
     {
-        private Animation animation;
-        private Camera camera;
-        [SerializeField]
+        #region props
+        private Transform camera;
+        private Camera cam1;
+        private Camera cam2;
         private Transform target;
+        private Transform keyframe;
+        private Transform keyframe2;
+        private Vector3 dest;
+        private Vector3 dest2;
+        #endregion
+
+        #region Init & Reset
+        private Vector3 initCamPos;
+        private Vector3 initCamRot;
+
+        private Vector3 initTargetPos;
+        private Vector3 initTargetRot;
+        private void Init()
+        {
+            target = transform.GetChild(0);
+            camera = transform.GetChild(1);
+            cam1 = camera.GetComponent<Camera>();
+            cam2 = transform.GetChild(0).GetComponentInChildren<Camera>();
+            keyframe = transform.GetChild(2);
+            keyframe2 = transform.GetChild(3);
+            initCamPos = camera.transform.position;
+            initCamRot = camera.transform.eulerAngles;
+
+            initTargetPos = target.position;
+            initTargetRot = target.eulerAngles;
+            dest = keyframe.position - target.position;
+            dest2 = keyframe2.position - keyframe.position;
+        }
+        private void Reset()
+        {
+            camera.transform.position = initCamPos;
+            camera.transform.eulerAngles = initCamRot;
+
+            target.position = initTargetPos;
+            target.eulerAngles = initTargetRot;
+
+            isTriggered = false;
+            isEntered = false;
+        }
+        #endregion
+
+        #region Resource
+
         [SerializeField]
         private Material skybox;
+        #endregion
 
-        private bool isTriggered;
-        public void Setter()
-        {
-            //스카이 박스 수정
-            RenderSettings.skybox = skybox;
-            //카메라 시작 세팅
-            Camera cam = Camera.main;
-            cam.transform.position = Vector3.zero;
-            cam.transform.rotation = Quaternion.Euler(-50f, -173f, -5f);
-        }
+        [SerializeField]
+        private bool isTriggered = false;
+        [SerializeField]
+        private bool isEntered = false;
         [ContextMenu("이히히")]
-        public async void Trigger()
+        public void Trigger()
         {
-            animation.Play();
-            await Task.Delay(5000);
-            // Code to execute after 5 seconds of delay
-
-
+            isTriggered = true;
         }
         private void Awake()
         {
-            animation = this.GetComponent<Animation>();
-            camera = Camera.main;
+            Init();
+            Reset();
         }
         private void Update()
         {
-            camera.transform.LookAt(target);
+            if (Vector3.Distance(keyframe.position, target.position) < 0.2f)
+            {
+                isEntered = true;
+                target.LookAt(keyframe2);
+                Invoke("CameraSwitch", 2f);
+            }
+            if (isEntered)
+            {
+                camera.LookAt(target);
+                target.position += target.forward * 50 * Time.deltaTime;
+                return;
+            }
+            if (isTriggered)
+            {
+                target.position += dest.normalized * 100 * Time.deltaTime;
+            }
+            return;
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                target.transform.position += dest.normalized * 100 * Time.deltaTime;
+                Debug.Log("키 눌림");
+            }
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                target.transform.position += dest2.normalized * 100 * Time.deltaTime;
+                Debug.Log("키 눌림");
+            }
+        }
+        private void CameraSwitch()
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
         }
     }
 }
